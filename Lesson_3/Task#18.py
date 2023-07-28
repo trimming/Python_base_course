88a14fccd3de9f2aa18995a0c17aa355651d65dc
Alexey Golikov <lexgolikoff@gmail.com>
2023-07-28 12:00:36 +0300

    add platform solutions

--- Lesson_3/Task#18.py
+++ Lesson_3/Task#18.py
@@ -27,3 +27,11 @@ for item in user_list:
         if item - number_search > 0: 
             print(item)
             break
+# Решение:
+# m = abs(k - list_1[0])  # модуль числа
+# number = list_1[0]
+# for i in range(1, len(list_1)):
+#     if m > abs(list_1[i] - k):
+#         m = abs(list_1[i] - k)
+#         number = list_1[i]
+# print(number)        
--- Lesson_3/Task#20.py
+++ Lesson_3/Task#20.py
@@ -30,4 +30,21 @@ for item in user_word:
     for k,v in data_dict.items():
         if item in k:
             count += int(v)
-print(count)            
+print(count) 
+
+# Решение:
+# k = 'ноутбук'
+# points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+# points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+# word = k.upper()  # переводим все буквы в верхний регистр
+# count = 0
+# for i in word:
+#     if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+#         for j in points_en:
+#             if i in points_en[j]:
+#                 count = count + j
+#     else:
+#         for j in points_en:
+#             if i in points_ru[j]:
+#                 count = count + j
+# print(count)
