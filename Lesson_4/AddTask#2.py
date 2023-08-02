409eb6ca96240e05ccf6f7f0ad044f8ddfa93ccb
Alexey Golikov <lexgolikoff@gmail.com>
2023-08-02 23:54:01 +0300

    add function get_main_list() for AddTask#2.py

--- Lesson_4/AddTask#2.py
+++ Lesson_4/AddTask#2.py
@@ -7,34 +7,8 @@
 # на выходе будет 5x^3 - x^2 + 4х - 7 = 0
 # можно использовать модуль re
 equation_1: str = "x^2 + 4*x + 5 = 0"
-equation_2: str = "5*x^3 - 3*x^2 - 12 = 0"
+equation_2: str = "5*x^3 - 3*x^2 - x + 12 = 0"
 
-# def get_dictionary(equation: str) -> list:
-#     res_dict: list = list(equation.split())
-#     temp_list: list = []
-#     for i in range(len(res_dict)):
-#         if "^" in res_dict[i]:
-#             temp_list.append(res_dict[i].split('*x^'))
-#         if "^" not in res_dict[i] and "*x" in res_dict[i]:
-#             temp_list.append(res_dict[i].split('*x'))
-#         if res_dict[i] == "+":
-#             continue       
-#         if res_dict[i] == "-":
-#             temp_list.append(res_dict[i])
-#         if res_dict[i] == '0':
-#             continue      
-#         try:
-#             temp_list.append(int(res_dict[i]))
-#         except:
-#             continue       
-#     for i in range(len(temp_list)):
-#         if temp_list[i] == '-':
-#             try:
-#                 temp_list[i + 1][0] = int(temp_list[i + 1][0])*-1
-#             except:
-#                 temp_list[i + 1] = temp_list[i + 1]*-1
-            
-#     print(temp_list)
 def get_temp_list(equation):
     temp_list = equation.split()
     j = 0
@@ -48,19 +22,71 @@ def get_temp_list(equation):
     temp_list.pop()                            
     return temp_list
         
-equation_list = get_temp_list(equation_1)
-print(equation_list)
+equation_list_1 = get_temp_list(equation_1)
+equation_list_2 = get_temp_list(equation_2)
 
 def get_dictionary(data_list):
     res_dict = {}
     for i in data_list:
-        if "^" in i:
-            res_dict[i] = i.split('*x^')    
-        else:    
-            res_dict[i] = i.split('*x') 
-        print(res_dict)
+        if "*x^" in i:
+            res_dict[i] = i.split('*x^')
+            res_dict[i][0] = int(res_dict[i][0])
+            res_dict[i][1] = int(res_dict[i][1])
+        elif "*x" in i:
+            res_dict[i] = i.split('*x')
+            res_dict[i][0] = int(res_dict[i][0])
+            res_dict[i][1] = int(res_dict[i][1] + '0')
+        elif "x^" in i:    
+            res_dict[i] = i.split('x^')
+            res_dict[i][0] = int(res_dict[i][0] + '1')
+            res_dict[i][1] = int(res_dict[i][1])
+        elif "x" in i:    
+            res_dict[i] = i.split('x')
+            res_dict[i][0] = int(res_dict[i][0] + '1')
+            res_dict[i][1] = int(res_dict[i][1] + '0')
+        else:
+            res_dict[i] = i.split()
+            res_dict[i][0] = int(res_dict[i][0])
+    resut_list = [v for k, v in res_dict.items()]
+    for i in resut_list:
+        if len(i) > 1:
+            i[0],i[1] = i[1],i[0]
+    print(resut_list)      
+    return resut_list
 
-get_dictionary(equation_list)
+res_list_1 = get_dictionary(equation_list_1)
+res_list_2 = get_dictionary(equation_list_2)
+
+def get_max_pow(list_1: list, list_2: list) -> int:
+    max_pow: int = max(list_1[0][0], list_2[0][0])
+    return max_pow
+
+
+pow: int = get_max_pow(res_list_1, res_list_2)
+print(pow)
+
+def get_main_list(list_1, list_2, max_pow):
+    main_list = list_1 + list_2
+    base_list = []
+    last_el = 0
+    first_el = 0
+    for i in range(len(main_list)):
+        if len(main_list[i]) == 1:
+            last_el += main_list[i][0]            
+        if main_list[i][0] == max_pow:
+            first_el += main_list[i][1]
+            
+        # elif main_list[i][0]    
+    base_list.append(first_el)
+    base_list.append(last_el)        
+   
+    print(main_list)    
+    print(base_list)    
+
+get_main_list(res_list_1, res_list_2, pow)
+
+
+
 
 
 # def get_list(equation: str) -> list:
@@ -93,19 +119,10 @@ get_dictionary(equation_list)
 #     return temp_list
 
 
-# list_equation_1: list = get_list(user_equation_1)
-# list_equation_2: list = get_list(user_equation_2)
-# print(list_equation_1)
-# print(list_equation_2)
 
 
-# def get_max_pow(list_1: list, list_2: list) -> int:
-#     max_pow: int = max(list_1[0][1], list_2[0][1])
-#     return int(max_pow)
 
 
-# pow: int = get_max_pow(list_equation_1, list_equation_2)
-# print(pow)
 
 
 # def get_result_list(list_1: list, list_2: list) -> list:
@@ -115,15 +132,7 @@ get_dictionary(equation_list)
 #         if type(temp_list[i]) == list and type(temp_list[i+1]) == list and i[1] == j[1]:
 #             result_list.append(int(i[0]) + int(j[0]))
 
-#     # for i in list_1:
-#     #     for j in list_2:
-#     #         if type(i) == list and type(j) == list and i[1] == j[1]:
-#     #             result_list.append(int(i[0]) + int(j[0]))
-#     #         if type(i) == list and type(j) == list and i[1] == '':
-#     #             result_list.append(int(i[0]))
-#     #         if type(i) == int and type(j) == int:
-#     #             result_list.append(i + j)
-#     print(temp_list)
 
 
-# get_result_list(list_equation_1, list_equation_2)
+
+
