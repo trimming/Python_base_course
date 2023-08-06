6e9cc3bfa0223a3fc1d42b535c0ee549b8ff0fb8
Alexey Golikov <lexgolikoff@gmail.com>
2023-08-06 14:22:48 +0300

    some update for AddTask#2.py

--- Lesson_4/AddTask#2.py
+++ Lesson_4/AddTask#2.py
@@ -6,11 +6,9 @@
 # например на входе 2x^2 + 4x + 5 = 0 и 5x^3 - 3*x^2 - 12 = 0
 # на выходе будет 5x^3 - x^2 + 4х - 7 = 0
 # можно использовать модуль re
-equation_1: str = "x^2 + 4*x + 5 = 0"
-equation_2: str = "5*x^3 - 3*x^2 - x + 12 = 0"
 
-def get_temp_list(equation):
-    temp_list = equation.split()
+def get_temp_list(equation): # Эта функция разделяет строку многочлена на элементы, удаляет +, = и 0, 
+    temp_list = equation.split() # а также если есть - меняет знак числа. Возвращает список с элементами многочлена.
     j = 0
     for i in temp_list:
         if (i == "+") or (i == "="):
@@ -22,115 +20,84 @@ def get_temp_list(equation):
     temp_list.pop()                            
     return temp_list
         
-equation_list_1 = get_temp_list(equation_1)
-equation_list_2 = get_temp_list(equation_2)
 
-def get_dictionary(data_list):
-    res_dict = {}
+def get_dictionary(data_list):# Эта функция принимает список сложенный из двух списков элементов многочленов, 
+    dictionary = {} # составляет словарь, в котором ключом является степень, возвращает этот словарь.
     for i in data_list:
         if "*x^" in i:
-            res_dict[i] = i.split('*x^')
-            res_dict[i][0] = int(res_dict[i][0])
-            res_dict[i][1] = int(res_dict[i][1])
-        elif "*x" in i:
-            res_dict[i] = i.split('*x')
-            res_dict[i][0] = int(res_dict[i][0])
-            res_dict[i][1] = int(res_dict[i][1] + '1')
-        elif "x^" in i:    
-            res_dict[i] = i.split('x^')
-            res_dict[i][0] = int(res_dict[i][0] + '1')
-            res_dict[i][1] = int(res_dict[i][1])
-        elif "x" in i:    
-            res_dict[i] = i.split('x')
-            res_dict[i][0] = int(res_dict[i][0] + '1')
-            res_dict[i][1] = int(res_dict[i][1] + '1')
-        else:
-            res_dict[i] = (i + ' 0').split(' ')
-            res_dict[i][0] = int(res_dict[i][0])
-            res_dict[i][1] = int(res_dict[i][1])
-    resut_list = [v for k, v in res_dict.items()]
-    # for i in resut_list:
-    #     if len(i) > 1:
-    #         i[0],i[1] = i[1],i[0]
-    print(resut_list)      
-    return resut_list
-
-res_list_1 = get_dictionary(equation_list_1)
-res_list_2 = get_dictionary(equation_list_2)
-
-def get_max_pow(list_1: list, list_2: list) -> int:
-    max_pow: int = max(list_1[0][0], list_2[0][0])
-    return max_pow
-
-
-pow: int = get_max_pow(res_list_1, res_list_2)
-print(pow)
-
-def get_main_list(list_1, list_2, max_pow):
-    main_list = list_1 + list_2
-    base_list = []
-    for i in list_2:
-        for j in list_1:
-            if i[1]==j[1]:
-                base_list.append(i[0] + j[0])
+            el = i.split("*x^")
+            key = int(el[1])
+            if key not in dictionary:
+                dictionary[key] = [int(el[0])]
             else:
-                base_list.append(i[0])
-                        
+                dictionary[key].append(int(el[0]))
+        elif "*x" in i:
+            el = i.split("*x")
+            key = 1
+            if key not in dictionary:
+                dictionary[key] = [int(el[0])]
+            else:
+                dictionary[key].append(int(el[0]))
+        elif "x^" in i:
+            el = i.split("x^")
+            key = int(el[1])
+            if key not in dictionary:
+                dictionary[key] = [int(el[0] + '1')]
+            else:
+                dictionary[key].append(int(el[0]))
+        elif "x" in i:
+            el = i.split("x")
+            key = 1
+            if key not in dictionary:
+                dictionary[key] = [int(el[0] + '1')]
+            else:
+                dictionary[key].append(int(el[0] + '1'))
+        else:
+            el = (i + ' 0').split(' ')
+            key = int(el[1])
+            if key not in dictionary:
+                dictionary[key] = [int(el[0])]
+            else:
+                dictionary[key].append(int(el[0]))              
+    return dictionary
 
-        
-    # print(main_list)    
-    print(base_list)    
+def get_result_list(data_dict):# Эта функция из словаря получает множетели и сохраняет их в список по принципу 
+    base_list = [] # соотношения ключа-степени индексу списка
+    temp = list(data_dict)
+    temp.sort(reverse=True)
+    for i in temp:        
+        base_list.append(sum(data_dict[i]))    
+    return base_list
 
-get_main_list(res_list_1, res_list_2, pow)
+def get_result(base_list):# В этой функции составляем результирующий многочлен
+    result = ""
+    k = len(base_list) - 1
+    for i in base_list:
+        if i == 0:
+            continue
+        elif i == 1 and k > 1:                
+            result += f"x^{k}"
+        elif i == 1 and k == 1:                    
+            result += f"x"    
+        else:         
+            if k == 0:
+                if i > 0:                
+                    result += f"+{i}"
+                else:
+                    result += f"{i}"    
+            elif k == 1:                
+                result += f"{i}*x"
+            else:               
+                result += f"{i}*x^{k}"
+        k -= 1    
+    result += " = 0"
+    print(result)
 
+equation_1: str = "2x^3 + x^2 + 5 = 0"
+equation_2: str = "5x^3 - 2x^2 - x + 12 = 0"
 
+res_dictionary = get_dictionary(get_temp_list(equation_1) + get_temp_list(equation_2))
 
-
-
-# def get_list(equation: str) -> list:
-#     res_dict: list = list(equation.split())
-#     temp_list: list = []
-#     for i in range(len(res_dict)):
-#         if "^" in res_dict[i]:
-#             temp_list.append(res_dict[i].split('*x^'))
-#         if "^" not in res_dict[i] and "*x" in res_dict[i]:
-#             temp_list.append(res_dict[i].split('*x'))
-#         if res_dict[i] == "+":
-#             continue
-#         if res_dict[i] == "-":
-#             temp_list.append(res_dict[i])
-#         if res_dict[i] == '0':
-#             continue
-#         try:
-#             temp_list.append(int(res_dict[i]))
-#         except:
-#             continue
-#     for i in range(len(temp_list)):
-#         if temp_list[i] == '-':
-#             try:
-#                 temp_list[i + 1][0] = int(temp_list[i + 1][0])*-1
-#             except:
-#                 temp_list[i + 1] = temp_list[i + 1]*-1
-#     for i in temp_list:
-#         if i == '-':
-#             temp_list.remove(i)
-#     return temp_list
-
-
-
-
-
-
-
-
-# def get_result_list(list_1: list, list_2: list) -> list:
-#     temp_list: list = list_1 + list_2
-#     result_list = []
-#     for i in range(len(temp_list)):
-#         if type(temp_list[i]) == list and type(temp_list[i+1]) == list and i[1] == j[1]:
-#             result_list.append(int(i[0]) + int(j[0]))
-
-
-
+get_result(get_result_list(res_dictionary))
 
 
