d17948e0aef859f79b19bf4465c9e90b5d8c72e2
Alexey Golikov <lexgolikoff@gmail.com>
2023-08-07 22:25:19 +0300

    added solutions for task#30 and task#32

--- Lesson_6/Task#32.py
+++ Lesson_6/Task#32.py
@@ -4,3 +4,11 @@
 # 33
 # 200
 # Ответ: [2, 3]
+from random import randint
+def get_list_indexes(data_list):
+    list1 =[data_list.index(i) for i in data_list if i < max and i > min]
+    return list1
+data_list = [randint(-10, 500) for _ in range(10, 30)]
+min = 1
+max = 200
+print(get_list_indexes(data_list))
