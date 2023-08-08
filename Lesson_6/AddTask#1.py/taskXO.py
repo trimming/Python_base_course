7d724d9ca8375f4283df20b993269025c09d78e6
Alexey Golikov <lexgolikoff@gmail.com>
2023-08-08 19:37:18 +0300

    some update for taskXO.py

--- Lesson_6/AddTask#1.py/taskXO.py
+++ Lesson_6/AddTask#1.py/taskXO.py
@@ -10,38 +10,42 @@
 # При ходе пользователя у надо спрашивать номер строки и столбца, куда он хочет сделать ход
 from random import randint
 from checkMatch import check_match
-area = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
-game = True
-move = True
-count = 0
-while game and count < 9:
-        if move:
-                row = int(input("Введите номер строки:\n"))
-                column = int(input("Введите номер столбца:\n"))
-                move = False
-                area[row-1][column-1] = 'X'
-                count += 1
-                print(*area[0], sep = ' | ')
-                print(*area[1], sep = ' | ')
-                print(*area[2], sep = ' | ')
-                print()
-                game = check_match(area[row-1][column-1],area)
-        else:
-                free_or_busy = True
-                while free_or_busy and count < 100:
-                        row = randint(1, 3)
-                        column = randint(1, 3)
-                        if area[row-1][column-1] == ' ':
-                                area[row - 1][column - 1] = 'O'
-                                free_or_busy = False
-                                print(*area[0], sep = ' | ')
-                                print(*area[1], sep = ' | ')
-                                print(*area[2], sep = ' | ')
-                                game = check_match(area[row-1][column-1],area)
-                        else:
-                                free_or_busy = True
-                move = True
-                count += 1
-if count == 9 and game == True:
-    print("Ничья!")
+def game_run(area):
+        game = True
+        move = True
+        count = 0
+        while game and count < 9:
+                if move:
+                        row = int(input("Введите номер строки:\n"))
+                        column = int(input("Введите номер столбца:\n"))
+                        move = False
+                        area[row-1][column-1] = 'X'
+                        count += 1
+                        print(*area[0], sep = ' | ')
+                        print(*area[1], sep = ' | ')
+                        print(*area[2], sep = ' | ')
+                        print()
+                        game = check_match(area[row-1][column-1],area)
+                else:
+                        free_or_busy = True
+                        while free_or_busy and count < 100:
+                                row = randint(1, 3)
+                                column = randint(1, 3)
+                                if area[row-1][column-1] == ' ':
+                                        area[row - 1][column - 1] = 'O'
+                                        free_or_busy = False
+                                        print(*area[0], sep = ' | ')
+                                        print(*area[1], sep = ' | ')
+                                        print(*area[2], sep = ' | ')
+                                        game = check_match(area[row-1][column-1],area)
+                                else:
+                                        free_or_busy = True
+                        move = True
+                        count += 1
+        if count == 9 and game == True:
+                print("Ничья!")
+data_list = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+game_run(data_list)
+
+
 
